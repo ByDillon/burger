62fb5ed912f5080d43ecaa90f971ca7ebe9f7f76
Dillon <dtidwell123@gmail.com>
2019-04-04 18:17:29 -0400

    upd8

--- config/connection.js
+++ config/connection.js
@@ -3,20 +3,6 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Dillon14',
-    database: 'burgers_db'
-  });
-};
-
-connection.connect();
-module.exports = connection;
-
-if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
